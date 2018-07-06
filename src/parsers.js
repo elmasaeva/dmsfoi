78d87842101458fde6c9ba286b1f09e296077785
Karida Sabr Nazari Masaeva <elmasaeva@protonmail.com>
2018-07-07 00:22:41 +0300

    better wording

--- src/parsers.js
+++ src/parsers.js
@@ -1,30 +1,30 @@
 export function packci4(ci4) {
     const [
         ,total,,
-        cul, spec, inv, sibl, ren, immsibl, ref, vict,
+        cul, spec, inv, sibl, former, immsibl, ref, vict,
         ,
-        marr, guard, torig, stint, zak, trans, st416,
+        marr, guard, torig, stint, zak, refg, ren,
     ] = ci4;
 
-    return {total, cul, spec, inv, sibl, ren, immsibl, ref, vict,
-        marr, guard, torig, stint, zak, trans, st416};
+    return {total, cul, spec, inv, sibl, former, immsibl, ref, vict,
+        marr, guard, torig, stint, zak, refg, ren};
 }
 
 export function packci5(ci5) {
     const [
         ,,,
         refused, total,
-        cul, spec, inv, sibl, ren, immsibl, vict,
+        cul, spec, inv, sibl, former, immsibl, vict,
         marr, guard, torig, stint, zak,
         pr,newpr,replacepr,
-        trans, st416,
+        refg, ren,
         refusedPr,
         recalled, rcit,  remigr, rother,
         recalledPr,
     ] = ci5;
 
-    return {refused, total, cul, spec, inv, sibl, ren, immsibl, vict,
-        marr, guard, torig, stint, zak, trans, st416,
+    return {refused, total, cul, spec, inv, sibl, former, immsibl, vict,
+        marr, guard, torig, stint, zak, refg, ren,
         pr, newpr, replacepr, refusedPr, recalled, rcit, remigr, rother,
         recalledPr};
 }
