b7160ded5d4cc77e984b34d271aa7bd8c0b4b036
Karida Sabr Nazari Masaeva <elmasaeva@protonmail.com>
2018-07-27 13:57:06 +0300

    show more data

--- src/Form52.js
+++ src/Form52.js
@@ -1,8 +1,15 @@
 import compareAsc from 'date-fns/compare_asc';
 import React, { Component } from 'react';
 import F52UA51 from './data/F52_UA51_2017.json';
+import F52UA26 from './data/F52_UA26_2017.json';
 import {code, flag} from './flags';
 import './Form52.css';
+
+function query(iso) {
+    if (iso === 'UA-51') return F52UA51;
+    if (iso === 'UA-26') return F52UA26;
+    return null;
+}
 
 function nform(n, form0, form1, form2) {
     return (n%10===1 && n%100!==11
@@ -12,16 +19,30 @@ function nform(n, form0, form1, form2) {
 }
 
 function parseDate(input) {
-    const [day, month, year] = input.split('.').map(Number);
+    const [day, month, year] = input ? input.split('.').map(Number) : [];
     return day ? new Date(year, month - 1, day) : null;
 }
 
-function weeks(date) {
-    return Math.floor(Number(date) / 1000 / 3600 / 24 / 7);
+function days(date) {
+    return Math.floor(Number(date) / 1000 / 3600 / 24);
+}
+
+function daysFrom(date1, date2) {
+    return days(date2) - days(date1);
 }
 
 function weeksFrom(date1, date2) {
-    return weeks(date2) - weeks(date1);
+    return Math.floor(daysFrom(date1, date2) / 7);
+}
+
+function formatPeriod(days) {
+    const years = Math.floor(days / 365);
+    let rdays = days % 365;
+    const months = Math.round(rdays / 30);
+    return [
+        years ? `${years} ${nform(years, 'рік', 'роки', 'роки')}` : null,
+        months ? `${months} ${nform(months, 'місяць', 'місяці', 'місяців')}` : null,
+    ].filter((part)=> part).join(', ');
 }
 
 function range(start, end) {
@@ -48,22 +69,48 @@ function Bar({line}) {
     return (<div className="line">
         <div style={{width: line.sweeks * FACTOR}} />
         <div style={{height: 20}}><pre>{line.sdate}</pre></div>
-        <div className={waitClass} style={{width: line.eweeks * FACTOR}} >{line.fcit}, {line.reason}, {line.eweeks} {nform(line.eweeks, 'тиждень', 'тиждня', 'тижнів')}</div>
+        <div className={waitClass} style={{width: line.eweeks * FACTOR}} >{line.fcit}, {line.reason}, {formatPeriod(line.edays)}</div>
         <div><pre>{line.edate} {line.dnum}</pre></div>
     </div>);
 }
 
+const REASONS = {
+    'Перебування понад 2 роки в шлюбі з громадянином України': 'шлюб',
+    'Безперервне проживання на законних підставах на території України протягом останніх п\'яти років.': '5р',
+};
+function reasonMap(reason) {
+    return REASONS[reason] || reason;
+}
+
+function unpackLine(line) {
+    if (line.length === 5) {
+        const [fcit, sdate, reason, dnum, edate] = line;
+        return {fcit, sdate, reason, dnum, edate};
+    }
+    if (line.length === 8) {
+        const [nn, fcountry, fcit, sdate, reason, edateNum, rdate, notes] = line;
+        const [dnum, edate] = edateNum === '-' ? [] : edateNum.split(' ');
+        return {nn, fcit, fcountry, sdate, reason: reasonMap(reason), dnum, edate, rdate, notes};
+    }
+    return {};
+
+}
+
 function Row({line, full}) {
-    const [fcit, sdate, reason, dnum, edate] = line;
+    const {nn, fcit, fcountry, sdate, reason, dnum, edate, rdate, notes} = unpackLine(line);
     return (<tr>
-        <td>—</td>
-        {full && <td colSpan={3} />}
+        <td>{nn || '—'}</td>
+        {full && <td colSpan={2} />}
+        {full && <td className="left">{flag(code())}&nbsp;{fcountry || '—'}</td>}
         <td className="left">{flag(code(fcit))}&nbsp;{fcit}</td>
         {full && <td colSpan={2} />}
         <td>{sdate}</td>
         {full && <td />}
         <td>{reason}</td>
         <td className="right">{dnum} {edate || '—'}</td>
+        <td className="right">{rdate || '—'}</td>
+        {full && <td colSpan={2} />}
+        {full && <td className="right">{notes || '—'}</td>}
     </tr>);
 }
 
@@ -80,7 +127,7 @@ function Header({full}) {
         {full && <th>Адреса реєстрації місця проживання особи</th>}
         <th>Підстави для прийняття до громадянства України (зазначити умови прийняття до громадянства України)</th>
         <th>Дата прийняття до громадянства України (номер і дата указу Президента України</th>
-        {full && <th>Відмітка про виконання особою зобов'язання припинити іноземне громадянство (дата подання декларації, довідка про вихід)</th>}
+        <th>Відмітка про виконання особою зобов'язання припинити іноземне громадянство (дата подання декларації, довідка про вихід)</th>
         {full && <th>Номер та дата видачі довідки про реєстрацію особи громадянином України</th>}
         {full && <th>Підпис особи про отримання довідки</th>}
         {full && <th>Примітки</th>}
@@ -88,7 +135,7 @@ function Header({full}) {
 }
 
 function Header2({full}) {
-    const cols = full ? range(1, 16) : [1, 5, 8, 10, 11];
+    const cols = full ? range(1, 16) : [1, 5, 8, 10, 11, 12];
     return (<tr>{cols.map((n)=> <th key={n}>{n}</th>)}</tr>)
 }
 
@@ -119,18 +166,22 @@ export class Form52 extends Component {
 
     renderBars(lines) {
         const y2016 = new Date(2016, 0, 1);
-        const dates = lines.map(
-            ([fcit, sdate, reason, dnum, edate])=> ({
+        const dates = lines
+            .map(unpackLine)
+            .map(({sdate, edate, rdate, ...rest})=> ({
+                ...rest,
                 start: parseDate(sdate), end: parseDate(edate),
-                reason, fcit, dnum,
                 sdate, edate,
-            })
-        ).map(({start, end, ...rest})=> ({
-            ...rest,
-            start, end,
-            sweeks: weeksFrom(y2016, start),
-            eweeks: weeksFrom(start, end),
-        })).sort((line1, line2)=> compareAsc(line1.start, line2.start));
+            }))
+            .map(({start, end, ...rest})=> ({
+                ...rest,
+                start, end,
+                sweeks: weeksFrom(y2016, start),
+                eweeks: weeksFrom(start, end),
+                edays: daysFrom(start, end),
+            }))
+            .sort((line1, line2)=> compareAsc(line1.start, line2.start));
+
         return (<div className="bars">
             {dates.map((line, idx)=> <Bar key={idx} line={line} />)}
         </div>);
@@ -139,7 +190,7 @@ export class Form52 extends Component {
     render() {
         const {iso} = this.props;
         const {full, bars} = this.state;
-        const lines = (iso === 'UA-51') ? F52UA51 : null
+        const lines = query(iso);
         if (!lines) {
             return null;
         }
